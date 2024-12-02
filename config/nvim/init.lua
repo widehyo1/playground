9988fda3244e33698f1a706956a0980ebae8278a
widehyo@gmail.com <widehyo@gmail.com>
2024-12-02 19:26:39 +0900

    nvim with clipboard

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,42 +1,20 @@
+--[[ custom configuration ]]--
+
 -- set leader key to comma
 vim.g.mapleader = ','
 -- esacpe key in insert mode
 vim.keymap.set('i', 'jj', '<ESC>')
 -- esacpe key in insert mode
 vim.keymap.set("n", "<leader>rc", ':e ~/.config/nvim/init.lua<CR>')
-
--- clipboard setting
-if string.find(vim.uv.os_uname().release, 'microsoft') ~= nil then
-    vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-        pattern = "*",
-        callback = function()
-            vim.system({ '/mnt/c/windows/system32/clip.exe' }, { stdin = vim.trim(vim.fn.getreg('"')) })
-        end,
-    })
-end
-
--- local osc52 = require("vim.ui.clipboard.osc52")
--- vim.g.clipboard = {
---   name = "OSC-52",
---   copy = {
---     ["+"] = osc52.copy("+"),
---     ["*"] = osc52.copy("*"),
---   },
---   paste = {
---     ["+"] = osc52.paste("+"),
---     ["*"] = osc52.paste("*"),
---   },
--- }
--- 
--- local silent_noremap = { silent = true, noremap = true }
--- vim.keymap.set('n', '<leader>y', '"+y', silent_noremap)
--- vim.keymap.set('v', '<leader>y', '"+y', silent_noremap)
--- 
--- vim.keymap.set('n', '<leader>d', '"+d', silent_noremap)
--- vim.keymap.set('v', '<leader>d', '"+d', silent_noremap)
--- vim.keymap.set('n', '<leader>D', '"+D', silent_noremap)
--- 
--- vim.keymap.set('n', '<leader>p', '"+p', silent_noremap)
+--[[ nvim tree ]]--
+-- toggle nvim tree
+vim.keymap.set("n", "<leader>n", ':NvimTreeToggle<CR>')
+vim.keymap.set("n", "<leader>a", ':NvimTreeFindFile<CR>')
+-- manage window
+vim.keymap.set('n', '<C-H>', '<C-W>h')
+vim.keymap.set('n', '<C-J>', '<C-W>j')
+vim.keymap.set('n', '<C-K>', '<C-W>k')
+vim.keymap.set('n', '<C-L>', '<C-W>l')
 
 -- basic settings
 vim.opt.hlsearch = false
@@ -51,26 +29,9 @@ vim.opt.expandtab = true
 vim.opt.list = true
 vim.opt.listchars = 'tab:>-,trail:-'
 
-treesitter_utils = require('treesitter_utils')
+-- clipboard setting
+-- install xclip
+vim.opt.clipboard = 'unnamedplus'
 
-vim.keymap.set('n', '<leader>t', treesitter_utils.print_curnode_lineage)
-
--- local current_node = vim.treesitter.get_node()
--- print(current_node:type())
--- print(current_node:parent())
--- types = {'dot_index_expression'}
--- print(vim.tbl_contains(types, current_node:type()))
--- print(current_node)
-
--- a = require('treesitter_utils')
--- print(a)
--- print(type(a))
--- print(a.find_node_ancestor)
--- print(a.find_node_ancestor({ 'chunk' }, current_node))
-
--- local chunk_node = require('treesitter_utils').M.find_node_ancestor({ 'chunk' }, current_node)
--- local chunk_node = M.find_node_ancestor({ 'chunk' }, current_node)
-
-
--- print(chunk_node:type())
-
+--[[ lazy ]]--
+require('config.lazy')
