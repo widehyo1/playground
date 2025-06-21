3d42fb30041966940380580d057e13c3e1f2f2bc
widehyo@gmail.com <widehyo@gmail.com>
2025-06-21 21:48:51 +0900

    itertools with roundrobin

---
+++ content_base/python/pure/roundrobin.py
@@ -0,0 +1,26 @@
+from operator import itemgetter
+from itertools import (
+    zip_longest as zl,
+    groupby,
+)
+
+SENTINEL = object()
+
+def gen_roundrobin(*iterables):
+    return (item for tup in zl(*iterables, fillvalue=SENTINEL)
+                 for item in tup if item is not SENTINEL)
+
+def gen_transpose(*iterables):
+    it_roundrobin_with_index = (
+        (idx, item)
+        for idx, tup in enumerate(zl(*iterables, fillvalue=SENTINEL))
+        for item in tup if item is not SENTINEL
+    )
+    for _colidx, it_col in groupby(it_roundrobin_with_index, itemgetter(0)):
+        yield list(item for _, item in it_col)
+
+a = [1, 2, 3]
+b = [4, 5]
+c = [6, 7, 8, 9]
+print(list(gen_roundrobin(a, b, c)))
+print(list(gen_transpose(a, b, c)))
