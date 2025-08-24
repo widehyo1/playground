4b51c72566e5bdce67ebeccbfdf70b2b330fd0bc
widehyo@gmail.com <widehyo@gmail.com>
2025-08-24 13:32:48 +0900

    r20, ch2: savepoint

--- reading/20_leehanyoung_Django_introduction/django_workspace/django_workspace/config/urls.py
+++ reading/20_leehanyoung_Django_introduction/django_workspace/django_workspace/config/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from config.views import main
+from config.views import main, burger_list
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", main),
+    path("burgers/", burger_list),
 ]
--- reading/20_leehanyoung_Django_introduction/django_workspace/django_workspace/config/views.py
+++ reading/20_leehanyoung_Django_introduction/django_workspace/django_workspace/config/views.py
@@ -1,4 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def main(request):
-    return HttpResponse("hello, I'm pyburger")
+    return render(request, "main.html")
+
+
+def burger_list(request):
+    return render(request, "burger_list.html")
