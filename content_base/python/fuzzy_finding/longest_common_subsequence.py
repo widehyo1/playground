72eb76fd2e849f767f76e0f84b52da2eb2481a33
widehyo@mobigen.com <widehyo@mobigen.com>
2025-09-20 13:38:58 +0900

    r23: necessary chapter only

---
+++ content_base/python/fuzzy_finding/longest_common_subsequence.py
@@ -0,0 +1,10 @@
+def longest_common_substring(text1: str, text2: str) -> tuple:
+    ...
+
+def longest_common_subsequence(text1: str, text2: str) -> tuple:
+    ...
+
+if __name__ == '__main__':
+    text1 = "abcdxyz"
+    text2 = "xyzabcd"
+    longest_common_substring(text1, text2)
